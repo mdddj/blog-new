e4603d94d3ba26f3b1967a49146161b8ef45b04a
mddd <hlxsmail@gmail.com>
2026-07-29 22:39:33 +0800

    更新依赖和修复社交图标使用内联 SVG

--- frontend/src/components/blog/public/index.tsx
+++ frontend/src/components/blog/public/index.tsx
@@ -15,7 +15,6 @@ import {
   Clock3,
   FileText,
   Folder,
-  Github,
   Hash,
   Loader2,
   Mail,
@@ -23,7 +22,6 @@ import {
   MessageCircle,
   Search,
   Sun,
-  Twitter,
   UserRound,
 } from "lucide-react";
 import { cn } from "@/lib/utils";
@@ -178,6 +176,10 @@ function BrandIcon({
 }
 
 const BRAND_ICON_PATHS = {
+  github:
+    "M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z",
+  twitter:
+    "M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z",
   zhihu:
     "M5.721 0C2.251 0 0 2.25 0 5.719V18.28C0 21.751 2.252 24 5.721 24h12.56C21.751 24 24 21.75 24 18.281V5.72C24 2.249 21.75 0 18.281 0zm1.964 4.078c-.271.73-.5 1.434-.68 2.11h4.587c.545-.006.445 1.168.445 1.171H9.384a58.104 58.104 0 01-.112 3.797h2.712c.388.023.393 1.251.393 1.266H9.183a9.223 9.223 0 01-.408 2.102l.757-.604c.452.456 1.512 1.712 1.906 2.177.473.681.063 2.081.063 2.081l-2.794-3.382c-.653 2.518-1.845 3.607-1.845 3.607-.523.468-1.58.82-2.64.516 2.218-1.73 3.44-3.917 3.667-6.497H4.491c0-.015.197-1.243.806-1.266h2.71c.024-.32.086-3.254.086-3.797H6.598c-.136.406-.158.447-.268.753-.594 1.095-1.603 1.122-1.907 1.155.906-1.821 1.416-3.6 1.591-4.064.425-1.124 1.671-1.125 1.671-1.125zM13.078 6h6.377v11.33h-2.573l-2.184 1.373-.401-1.373h-1.219zm1.313 1.219v8.86h.623l.263.937 1.455-.938h1.456v-8.86z",
   weibo:
@@ -293,8 +295,8 @@ export function PublicFooter() {
   const { config } = useSiteConfig();
   const currentYear = new Date().getFullYear();
   const socialLinks = [
-    { href: config.social_github, label: "GitHub", icon: <Github className="h-4 w-4" /> },
-    { href: config.social_twitter, label: "Twitter", icon: <Twitter className="h-4 w-4" /> },
+    { href: config.social_github, label: "GitHub", icon: <BrandIcon title="GitHub" path={BRAND_ICON_PATHS.github} /> },
+    { href: config.social_twitter, label: "Twitter", icon: <BrandIcon title="Twitter" path={BRAND_ICON_PATHS.twitter} /> },
     { href: config.social_telegram, label: "Telegram", icon: <MessageCircle className="h-4 w-4" /> },
     { href: config.social_weibo, label: "微博", icon: <BrandIcon title="Sina Weibo" path={BRAND_ICON_PATHS.weibo} /> },
     { href: config.social_zhihu, label: "知乎", icon: <BrandIcon title="Zhihu" path={BRAND_ICON_PATHS.zhihu} /> },
